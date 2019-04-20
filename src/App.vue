81155cbc69487e23e036aa4869eaa4e7ffcffcde
kinogam <kinogam@gmail.com>
2019-04-20 20:45:44 +0800

    fixed material font

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+      <div>
+          <small>Raised</small>
+          <md-button class="md-raised">Default</md-button>
+          <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
+          <md-button class="md-raised md-primary">Primary</md-button>
+          <md-button class="md-raised md-accent">Accent</md-button>
+          <md-button class="md-raised" disabled>Disabled</md-button>
+      </div>
+
+      <div>
+          <div>
+              <small>Regular</small>
+              <md-button class="md-fab">
+                  <md-icon>menu</md-icon>
+              </md-button>
+              <md-button class="md-fab md-primary">
+                  <md-icon>add</md-icon>
+              </md-button>
+              <md-button class="md-fab md-plain">
+                  <md-icon>edit</md-icon>
+              </md-button>
+          </div>
+
+          <div>
+              <small>Mini/Dense</small>
+              <md-button class="md-fab md-mini">
+                  <md-icon>menu</md-icon>
+              </md-button>
+              <md-button class="md-fab md-mini md-primary">
+                  <md-icon>add</md-icon>
+              </md-button>
+              <md-button class="md-fab md-mini md-plain">
+                  <md-icon>edit</md-icon>
+              </md-button>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -24,5 +60,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+small {
+    display: block;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import { MdButton, MdContent, MdTabs, MdIcon } from 'vue-material/dist/components';
+
+import './assets/css/material.design.css';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+Vue.use(MdIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
